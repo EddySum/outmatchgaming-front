fe1d20d92d31e4837a5d8f8e2dc8e852e2ec5624
Eddy Sumra <adnansumra115@gmail.com>
2020-04-26 03:05:01 -0400

    created layout and top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
+import 'antd/dist/antd.css';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Layout, Menu} from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu className="menu" theme="dark" mode="horizontal">
+          <Menu.Item key="1">Games</Menu.Item>
+          <Menu.Item key="2">Tournaments</Menu.Item>
+          <Menu.Item key="3">Leaderboards</Menu.Item>
+
+          <Menu.Item className="login" key="4">
+            <a href="http://localhost:3000/login">
+              Login / Sign Up
+            </a>
+            </Menu.Item>
+        </Menu>
+      
+        
+      </Header>
+
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+      
+      
+      
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>Out Match Gaming ©2018 Created by Eddy Productions</Footer>
+    </Layout>
   );
 }
 

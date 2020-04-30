3a27031d83a73054e6472f87aaa764908210067e
Eddy Sumra <adnansumra115@gmail.com>
2020-04-30 04:13:55 -0400

    query game by id and provide view for its ladders

--- src/components/GameDetail/GameDetail.tsx
+++ src/components/GameDetail/GameDetail.tsx
@@ -1,29 +1,64 @@
 import 'antd/dist/antd.css';
 import './GameDetail.css';
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
 
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 
-const GET_GAMES = gql`
-  query {	
-    getGames {
+import { Card, Col, Row, PageHeader} from 'antd';
+
+export const GET_GAME_DETAILS = gql`
+  query GameDetails($gameId: ID!) {
+    getGame(_id: $gameId) {
       _id
       name
-      platform,
-      imageLink
+      platform
+      ladders {
+        _id
+        name
+        maxPlayers
+      }
     }
   }
 `;
 
 function GameDetail() {
-  const { loading, error, data } = useQuery(GET_GAMES);
+  const history = useHistory();
+
+  const url = window.location.href;
+  const gameId = url.slice(url.lastIndexOf('/')+1)
+  
+  const { data, loading, error } = useQuery(GET_GAME_DETAILS, { variables: { gameId } } );
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>404 Not Found</p>;
-   
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+
   return (
-    <p>testing</p>
+    <div className="game-wrapper">
+      <PageHeader
+        className="game-header"
+        onBack={() => history.goBack()}
+        title={data.getGame.name}
+        subTitle="Ladders / Tournaments"
+      />
+      <Row gutter={[32, 32]}>
+        {data.getGame.ladders?.map(({ name, maxPlayers }: any) => (
+          <Col>
+            <Card 
+              className="ladder-card" 
+              title={name} 
+              extra={<Link to="/">Create Team</Link>}
+            >
+              <p>Type: Ladder</p>
+              <p>Max Players: {maxPlayers}</p>
+            </Card> 
+          </Col>
+        ))}
+      </Row>
+    </div>
   )
 }
 

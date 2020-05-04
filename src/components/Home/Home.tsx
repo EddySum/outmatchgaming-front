14d8482c38c3f412a855013661771650ad949176
Eddy Sumra <adnansumra115@gmail.com>
2020-05-04 03:17:20 -0400

    Added Carousel To Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,11 +2,19 @@ import 'antd/dist/antd.css';
 import './Home.css';
 import React from 'react';
 
+import { Carousel } from 'antd';
+
 import GamesHome from '../GamesHome/GamesHome';
 
 function Home() {
   return (
-    <GamesHome/>
+    <div className="home-wrapper">
+      <Carousel>
+        <div className="splash" style={{ backgroundImage: `url("https://i.ibb.co/7Gb5k7G/splash1.jpg")` }}/>
+        <div className="splash" style={{ backgroundImage: `url("https://i.ibb.co/7Gb5k7G/splash1.jpg")` }}/>
+      </Carousel>
+      <GamesHome/>
+    </div>
   );
 }
 
